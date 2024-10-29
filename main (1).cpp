8b4e924d3e803fb0f4061ef1edd9f6481c4d9d3a
nazwaaurellia29 <nazwaaurellia29@gmail.com>
2024-10-29 11:57:58 +0700

    Add files via upload
    belajar bersama minggu pertama

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, hasil;
+
+    cout << "Masukkan bilangan pertama: ";
+    cin >> a;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> b;
+
+    hasil = a * b;
+
+    cout << "Hasil perkalian: " << hasil << endl;
+
+    return 0;
+}
---
+++ main (2).cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+	string nama,kampus,prodi,kelas;
+	double angkatan;
+	
+	cout << "masukkan nama anda : " ;
+	getline(cin,nama);
+	cout <<"masukkan kampus anda : ";
+	cin>> kampus;
+	cout <<"masukkan nama prodi  :  ";
+	cin>> prodi;
+	cout<<"dari angkatan berapa  :  ";
+	cin>>angkatan;
+	cout<<"dari kelas berapa     :";
+	cin>>kelas;
+	
+	cout<<endl<<endl<<endl;
+	cout<<"hallow nama saya "<<nama<<endl<<endl;
+	cout<<"Kampussss  : "<<kampus<<endl;
+	cout<<"Prodi      : "<<prodi<<endl;
+	cout <<"angkatan  : "<<angkatan<<endl;
+	cout<<"kelas       :"<<kelas<<endl<<endl;
+	cout<<"salaam kenall yaaa";
+	
+	
+	
+	
+	
+	
+	
+
+
+}
